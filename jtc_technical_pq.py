5bf9c82c678571f85d81d6566eb0df38de6510ea
ecdj4008 <124374642+ecdj4008@users.noreply.github.com>
2023-09-15 20:24:40 -0400

    finished w2d4

--- jtc_technical_pq.py
+++ jtc_technical_pq.py
@@ -1,9 +1,9 @@
-# jtc_technical_pq.py
+#jtc_technical_pq.py
 
 def jtc_technical():
     print('Hello World?')
-    
-    
+
+
 #jtc_technical()
 
 '''
@@ -15,11 +15,11 @@ Step 1
 Step 2
     'run' your code in your terminal by
         1. 'cd' into the folder into which this file is located
-        2. In the terminal, run your your computer's version of `python3 hello.py` and hitting enter
+        2. In the terminal, run your computer's version of `python3 jtc_technical_pq.py` and hit enter
 
 Step 3
-        Using a single-line comment and no other characters, comment out Line 7
+    Using a single-line comment and no other characters, comment out Line 7
 
 Step 4
-        Delete pass on Line 5
+    Delete the 'pass' statement on Line 5
 '''
